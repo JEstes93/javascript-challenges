9c6ff347b74aec78100de0649350c1894b5d8004
heroes-coding <jeremy@heroes.report>
2018-11-14 10:46:37 -0600

    add-eventing and promise.all

--- promise-dot-all/promise-dot-all.js
+++ promise-dot-all/promise-dot-all.js
@@ -1,5 +1,19 @@
-function all (promises) {
-  return {}
-}
+const all = (promises) => new Promise((resolve, reject) => {
+  const values = []
+  const getValue = (promise,i) => {
+    if (!promise) {
+      resolve(values)
+    } else {
+      promise.then(v => {
+        values.push(v)
+      }).catch(e => values.push(e))
+      .finally(() => {
+        i++
+        getValue(promises[i],i)
+      })
+    }
+  }
+  getValue(promises[0],0)
+})
 
 module.exports = all
