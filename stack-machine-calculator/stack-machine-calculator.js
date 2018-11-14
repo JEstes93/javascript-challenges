3190b1197d1f1008a370a5662f366ae72dd8b0ef
Kalkazar <joecurrystodder@gmail.com>
2018-11-14 11:16:38 -0600

    stack-machine-calculator solution: passes all tests

--- stack-machine-calculator/stack-machine-calculator.js
+++ stack-machine-calculator/stack-machine-calculator.js
@@ -1,5 +1,24 @@
 const stackMachineCalculator = (instructions) => {
-  return instructions
+  let stringArray = instructions.split(' ')
+  let stack = []
+  for(let i in stringArray) {
+    if(!isNaN(stringArray[i])) {
+      stack.push(Number(stringArray[i]))
+    }
+    if(stringArray[i] === "+" || stringArray[i] === "ADD") {
+      stack.push(stack.pop() + stack.pop())
+    }
+    if(stringArray[i] === "-" || stringArray[i] === "SUB") {
+      stack.push(stack.pop() - stack.pop())
+    }
+    if(stringArray[i] === "POP") {
+      stack.pop()
+    }
+    if(stringArray[i] === "DUP") {
+      stack.push(stack[stack.length-1])
+    }
+  }
+  return stack.pop()
 }
 
 module.exports = stackMachineCalculator
